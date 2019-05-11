627781e86ad730f04e33faa4e8f51ea8f8e814f9
BacchusKC <bacchuskc@gmail.com>
2019-05-11 02:10:36 -0500

    search

--- components/search-menu-items/SearchMenuItems.js
+++ components/search-menu-items/SearchMenuItems.js
@@ -3,79 +3,129 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown'
 
 const buttonStyle = {
-    border: '2px solid black'
-}
+    borderTop: '2px solid black',
+    borderLeft: '2px solid black',
+    borderBottom: '2px solid black',
+    backgroundColor: '#343a40',
+    height: '50px'
+};
+
 
 const SearchMenuItems = (props) => (
     <Fragment>
-            <div className='input-group'>
-                <DropdownButton id='dropdown-basic-button' name='searchType' variant='default' style={buttonStyle} title='Search'>
-                    <div className='search'>
-                        <i className='closeButton far fa-window-close' onClick={props.clickClose}></i>
-                        <div className='form-group inputs'>
-                            <div className='form-group inputs'>
-                                <label htmlFor='menuSearchQuery'>Menu Search</label>
-                                <input type='text' className='form-control' name='menuSearchQuery' value={props.searchValueQuery} placeholder='(e.g. Doritos Locos Tacos)' id='menuSearchQuery' onChange={(e) => props.formStateChange(e)} />
+        <div className='jumbotron jumbotron-fluid search-field-jumbotron'>
+            <div className='container'>
+                <div className='row'>
+                    <h3 className='search-jumbotron-title'>What would you like to search?</h3>
+                </div>
+                <form onSubmit={(e) => props.btnClickMenu(e)}>
+                    <div className='form-row'>
+                        <div className='container'>
+                            <div className='input-group'>
+                                <div className='input-group-prepend'>
+                                    <DropdownButton id='dropdown-search-selector' variant='default' style={buttonStyle} onSelect={(e) => props.typeStateChange(e)} title='Menu Items'>
+                                        <Dropdown.Item eventKey='recipes'>Recipes</Dropdown.Item>
+                                        <Dropdown.Item eventKey='grocery'>Grocery Items</Dropdown.Item>
+                                        <Dropdown.Item eventKey='menu'>Menu Items</Dropdown.Item>
+                                    </DropdownButton>
+                                </div>
+                                <input type='text' className='form-control bg-light' name='menuSearchQuery' value={props.searchValueQuery} id='menuSearchQuery' placeholder='(e.g. Doritos Locos Tacos)' onChange={(e) => props.formStateChange(e)}></input>
                             </div>
                         </div>
-                        <div className='input-group-append'>
-                            <button type='button' className='btn btn-outline-dark col' id='menuQuerySearchButton' onClick={props.btnClickMenu}>Search</button>
+                    </div>
+                    <div className='form-row search-btn-container'>
+                        <div className='container'>
+                            <button type='submit' className='btn btn-dark btn-block search-submit-btn' id='menuQuerySearchButton' onClick={(e) => props.btnClickMenu(e)}>Search</button>
                         </div>
                     </div>
-                </DropdownButton>
-                <div className='input-group-prepend'>
-                    <DropdownButton id='dropdown-basic-button' name='searchType' variant='default' style={buttonStyle} onSelect={(e) => props.typeStateChange(e)} title='Menu Items'>
-                        <Dropdown.Item eventKey='recipes'>Recipes</Dropdown.Item>
-                        <Dropdown.Item eventKey='grocery'>Grocery Items</Dropdown.Item>
-                        <Dropdown.Item eventKey='menu'>Menu Items</Dropdown.Item>
-                    </DropdownButton>
-                </div>
-                <style jsx>{`
-                    input {
-                    width: 100%;
-                    margin: auto;
-                    height: 50px;
-                    font-size: 20px;
-                    border: 2px solid black;
-                    padding: 7px;
-                    }
-                    .inputs {
-                    margin: auto;
-                    width: 90%;
-                    }
-                    .input-group {
-                    max-width: 1070px;
-                    margin: auto;
-                    margin-top: 25px;
-                    }
-                    label {
-                    height: 100%;
-                    width: 100%;
-                    font-size: 20px;
-                    font-weight: bolder;
-                    color: black;
-                    text-align: center;
-                    }
-                    #menuQuerySearchButton {
-                    margin: 20px 7px 7px 7px;
-                    }
-                    .search {
-                    min-width: 75vw;
-                    max-width: 90vw;
-                    position: relative;
-                    }
-                    .closeButton {
-                    font-size: 30px;
-                    position: absolute;
-                    right: 1%;
-                    top: 0.3%;
-                    color: black;
-                    }
-                    .closeButton:hover {
-                    cursor: pointer;
-                    }
-        `       }</style>
+
+
+                </form>
             </div>
+        </div>
+        <style jsx>{`
+                                    .search-submit-btn {
+                                        margin: 0 auto;
+                                        margin-top: 25px;
+                                    }
+
+                                    .refine-search-btn {
+                                        margin: 0 auto;
+                                        margin-top: 25px;
+                                    }
+
+                                    .search-jumbotron-title {
+                                        text-align: center;
+                                        margin: 0 auto;
+                                        margin-bottom: 25px;
+                                    }
+
+                                    .collapse-container {
+                                        width: 100%;
+                                    }
+
+
+                                    input {
+                                        width: 100%;
+                                        margin: auto;
+                                        height: 50px;
+                                        font-size: 20px;
+                                        border: 2px solid black;
+                                        padding: 7px;
+                                    }
+                                    .dropdowns {
+                                        height: 50px;
+                                        font-size: 20px;
+                                    }
+                                    label {
+                                        height: 100%;
+                                        width: 100%;
+                                        font-size: 20px;
+                                        font-weight: bolder;
+                                        color: black;
+                                        text-align: center;
+                                    }
+                                    .inputs {
+                                        margin: auto;
+                                        width: 90%;
+                                    }
+                                    .itemDrop {
+                                        width: fit-content;
+                                        width: 90%;
+                                        margin: 5px auto!important;
+                                    }
+                                    
+                                    .input-group {
+                                        max-width: 1070px;
+                                        margin: auto;
+                                        margin-top: 25px;
+                                    }
+                                    .btn-group {
+                                        width: 90%;
+                                    }
+                                    .searchMobile {
+                                        min-width: 95vw;
+                                        max-width: 90vw;
+                                        padding-top: 10px;
+                                        position: relative;
+                                    }
+                                    .searchDesktop {
+                                        min-width: 60vw;
+                                        padding-top: 10px;
+                                        position: relative;
+                                    }
+                                    .closeButton {
+                                        font-size: 30px;
+                                        position: absolute;
+                                        right: 1%;
+                                        top: 0.3%;
+                                        color: black;
+                                    }
+                                    .closeButton:hover {
+                                        cursor: pointer;
+                                    }
+                                `}</style>
+
     </Fragment>
 )
 
